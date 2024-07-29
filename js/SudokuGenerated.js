931ae8534ad8c8947ee888753c7a8da638c97332
OuterCyrex <167676294+OuterCyrex@users.noreply.github.com>
2024-07-29 19:17:39 +0800

    Add files via upload

---
+++ js/SudokuGenerated.js
@@ -0,0 +1,155 @@
+class Sudoku {
+
+    constructor() {
+        this.digits = this.blankMatrix(9);
+    }
+
+    blankMatrix(size) {
+        let newMatrix = [];
+        for (let i = 0;i < size;i ++) {
+            newMatrix.push([]);
+        }
+        return newMatrix;
+    }
+
+    makeDigits() {
+        let colLists = this.blankMatrix(9);
+        let areaLists = this.blankMatrix(3);
+        let nine = this.randNine();
+        let i = 0,
+            j = 0,
+            areaIndex = 0,
+            count = 0,
+            error = false,
+            first = 0;
+        for (i = 0;i < 9;i ++) {
+            colLists[i].push(nine[i]);
+        }
+        areaLists[0] = nine.slice(0, 3);
+        areaLists[1] = nine.slice(3, 6);
+        areaLists[2] = nine.slice(6);
+
+        for (i = 0;i < 8;i ++) {
+            nine = this.randNine();
+            if (i % 3 == 2) {
+                areaLists = this.blankMatrix(3);
+            }
+
+            for (j = 0;j < 9;j ++) {
+                areaIndex = Math.floor(j / 3);
+                count = 0;
+                error = false;
+                while (colLists[j].includes(nine[0]) || areaLists[areaIndex].includes(nine[0])) {
+                    if (++count >= nine.length) {
+                        error = true;
+                        break;
+                    }
+                    nine.push(nine.shift());
+                }
+                if (error) return false;
+                first = nine.shift();
+                colLists[j].push(first);
+                areaLists[areaIndex].push(first);
+            }
+        }
+        this.digits = colLists;
+        return true;
+    }
+
+    randNine() {
+        const nine = this.nine();
+        let index = 0;
+
+        for (let i = 0;i < 5;i ++) {
+            index = this.randIndex();
+            [nine[0], nine[index]] = [nine[index], nine[0]];
+        }
+
+        return nine;
+    }
+
+    nine() {
+        return [1, 2, 3, 4, 5, 6, 7, 8, 9];
+    }
+
+    randIndex() {
+        return Math.floor(Math.random() * 9);
+    }
+}
+
+function getRandomInt(n) {
+    return Math.floor(Math.random() * n);
+}
+
+var AllSudoku = new Sudoku();
+
+const btn1 = document.querySelector("#btn1");
+const btn2 = document.querySelector("#btn2");
+
+btn1.addEventListener("click",function() {
+    let hard = prompt("请输入数独的难度：","20")
+    window.sudoku = AllSudoku
+    const start = Date.now();
+    while (! sudoku.makeDigits());
+    const end = Date.now();
+    console.log(end - start);
+    for (let i = 1; i <= 9; i ++) {
+        for(let j = 1; j <= 9; j ++) {
+            let block = `.part${i} .block${j}`
+            let dom = document.querySelector(block)
+            dom.innerText = sudoku.digits[i-1][j-1];
+            dom.style.color="black";
+            dom.style.fontWeight="normal";
+        }
+    }
+    let basic = 20;
+    basic += hard * 4
+    for(let i = 0;i < basic;i++){
+        let row = getRandomInt(10);
+        let col = getRandomInt(10);
+        let block = `.part${row} .block${col}`
+        let spaceBlock = document.querySelector(block)
+        if (spaceBlock) {
+            spaceBlock.innerText = '';
+        }
+    }
+})
+
+btn2.addEventListener("click", function() {
+    for (let i = 1; i <= 9; i ++) {
+        for(let j = 1; j <= 9; j ++) {
+            let block = `.part${i} .block${j}`
+            let dom = document.querySelector(block)
+            dom.innerText = sudoku.digits[i-1][j-1];
+        }
+    }
+})
+
+let subBlock = document.querySelector("#SudokuMain")
+subBlock.addEventListener("click", function(e) {
+    let row = e.target.parentNode.getAttribute("class")
+    let col = e.target.getAttribute('class')
+    let input = prompt("请输入要输入的数字")
+    let block = `.part${row[row.length-1]} .block${col[col.length-1]}`
+    let output = document.querySelector(block)
+    output.innerText = input;
+    output.style.color="orange";
+    output.style.fontWeight = "bold";
+
+    let count = 0;
+    for (let i = 1; i <= 9; i ++) {
+        for(let j = 1; j <= 9; j ++) {
+            let block = `.part${i} .block${j}`
+            let dom = document.querySelector(block)
+            if(dom.innerText[0] == sudoku.digits[i-1][j-1]){
+                count++;
+            }
+        }
+    }
+    console.log(count);
+    if(count === 81){
+        alert("完全正确喵！")
+        window.location.reload();
+    }
+})
+
